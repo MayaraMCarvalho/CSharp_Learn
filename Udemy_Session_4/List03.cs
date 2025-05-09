4dbae6af78067edd37b0c52aaf7dcb17dbb4f6c3
Mayara Carvalho <mayara.carvalho@mycon.com.br>
2025-05-09 17:31:55 -0300

    feat: Adicionar classe Produto da lista de atividades da Sessão 4

---
+++ Udemy_Session_4/List03.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace List03
+{
+   public class Cotacao()
+    {
+        public double   CotacaoDolar;
+        public double   QuantidadeDolar;
+
+        double RealToDolar()
+        {
+            double total = CotacaoDolar * QuantidadeDolar;
+            return total += total * 0.06 ;
+        }
+
+        public void ValorTotal()
+        {
+            Console.WriteLine($"Valor a ser pago em reais = " +
+                $"{RealToDolar().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+
+    public class Produto
+    {
+        public string   Nome;
+        public double   Preco;
+        public int      Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdcionarProdutos(int quantity)
+        {
+           Quantidade += quantity;
+        }
+
+        public void RemoverProdutos( int quantity )
+        {
+            Quantidade -= quantity;
+         }
+
+        public override string ToString()
+        {
+            return $"{Nome}, R$ {Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                   $"{Quantidade} unidades, Total: " +
+                   $"{ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- Udemy_Session_4/Program.cs
+++ Udemy_Session_4/Program.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using List01;
 using List02;
+using List03;
 
 namespace Udemy_Session_4
 {
@@ -16,7 +17,11 @@ namespace Udemy_Session_4
             // Exs List02
             //Retangulos();
             //Impostos();
-            Notas();
+            //Notas();
+
+            // Exs List03
+            //Cotações();
+            Produtos();
         }
 
         // "Mains" Exs List01
@@ -30,13 +35,13 @@ namespace Udemy_Session_4
             p1.Nome = Console.ReadLine();
             Console.Write("Idade: ");
             p1.Idade = int.Parse(Console.ReadLine());
-            
+
             Console.WriteLine("\nDados da segunda pessoa:");
             Console.Write("Nome: ");
             p2.Nome = Console.ReadLine();
             Console.Write("Idade: ");
             p2.Idade = int.Parse(Console.ReadLine());
-            
+
             if (p1.Idade == p2.Idade)
             {
                 Console.WriteLine($"\n{p1.Nome} e {p2.Nome} têm a mesma idade.");
@@ -80,7 +85,7 @@ namespace Udemy_Session_4
             Console.WriteLine("Entre com a largura e altura do retângulo:");
             r1.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             r1.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                     
+
             Console.WriteLine($"\nAREA = {r1.Area().ToString("F2", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"PERÍMETRO = {r1.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"DIAGONAL = {r1.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
@@ -99,7 +104,7 @@ namespace Udemy_Session_4
             func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine($"\nFuncionário: {func}");
-            
+
             Console.Write("\nDigite a porcentagem para aumentar o salário: ");
             double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             func.AumentarSalario(porcentagem);
@@ -119,6 +124,47 @@ namespace Udemy_Session_4
             aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine($"\nNOTA FINAL = {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
             aluno.Situacao();
+        }
+
+        // "Mains" Exs List03
+        static void Cotações()
+        {
+            Cotacao cotacao = new Cotacao();
+            Console.Write("Qual é a cotação do dolar? ");
+            cotacao.CotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            cotacao.QuantidadeDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            cotacao.ValorTotal();
+        }
+
+        static void Produtos()
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome do produto: ");
+            produto.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nDados do produto: {produto}");
+
+            Console.Write("\nDigite o número de produtos a ser adicionado ao estoque: ");
+            int quantity = int.Parse(Console.ReadLine());
+            produto.AdcionarProdutos(quantity);
+            Console.WriteLine($"\nDados atualizados: {produto}");
+
+            Console.Write("\nDigite o número de produtos a ser removido do estoque: ");
+            quantity = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(quantity);
+
+            Console.WriteLine($"\nDados atualizados: {produto}");
         }
     }
 }
